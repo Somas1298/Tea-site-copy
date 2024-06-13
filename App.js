e1eb7ea8f3a65d4c5b3a6f9b21e0264b520aa4bb
Somasundharam <somasundharam.1@outlook.com>
2024-06-13 06:05:21 -0700

    Add files via upload

---
+++ App.js
@@ -0,0 +1,85 @@
+import pho from'./81gxUxmADJL._AC_SL1500.jpg'
+import pho1 from'./06673-teahouseexclusives-p.png'
+import pho2 from'./d4be8d102758fc9c74560f5128b9b3b8--tea-station-coffee-stations.jpg'
+import pho3 from'./OIP.jpeg'
+import pho4 from './9be81644b27f258b59c149fddd012523.jpg'
+import { HiArrowNarrowLeft } from "react-icons/hi";
+import { HiArrowNarrowRight } from "react-icons/hi";
+import pho5 from './Q2_2023_-_HOMEPAGE_DESKTOP_-_Sub_Starter_Kit_header_2.webp';
+import pho6 from './HOMEPAGE_-_Cold_Brew_2024_-_desktop_header_2_d6b9fc58-2339-443f-9310-aad098f2b97e.webp'
+import './App.css';
+
+function App() {
+ 
+  return (
+<div>
+<lable id="top">Free 24hrs shipping over $20.Free 24hrs Courier over $75</lable>
+<div id="mid">
+<lable id= "layer"> BREW TEA CO </lable>
+<button id="layer1">Subscribe</button>
+<lable id="layer2">Shop</lable>
+<lable  id="layer2">The Gift Shop</lable>
+<lable  id="layer2">Brew Coins</lable>
+<lable  id="layer2">About</lable>
+<button id="layer3">Search</button>
+<button id="layer4">Login</button>
+</div>
+<div id='add'>
+<img src={pho5} alt='prop' id='pic8'></img>
+<label id='bee'>This Is What </label>
+<label id='bee1'> Proper Tea </label>
+<label id='bee2'> Tastes Like</label>
+<label id='bee3'>Everything you need to brew perfectly. All in the same box.</label>
+<button id='bee4'>Start a Subscribe</button>
+<div id='erra1'>
+<HiArrowNarrowLeft id='ar'/> 
+<HiArrowNarrowRight id='ar'/>
+</div>
+</div>
+<div id="end">
+<lable id="layer5">Gift & Bundles</lable>
+<a href="#" id="layer6">Shop all</a>
+<div id="err">
+<div className='one'>
+<img src={pho} alt='prop' id='pic1'></img>
+<p id="era1">Tea Set - from $30</p>
+</div>
+<div className='one'>
+<img src={pho2} alt='prop' id='pic2'></img>
+<p id="era2">Tea flaver Set - from $30</p>
+</div>
+<div className='one'>
+<img src={pho} alt='prop' id='pic3'></img>
+<p id="era3">Tea  Set - from $30</p>
+</div>
+<div className='one'>
+<img src={pho2} alt='prop' id='pic4'></img>
+<p id="era4">Tea flaver Set - from $30</p>
+</div>
+<div className='one'>
+<img src={pho} alt='prop' id='pic5'></img>
+<p id="era5">Tea Set - from $30</p>
+</div>
+<div className='one'>
+<img src={pho} alt='prop' id='pic6'></img>
+<p id="era6">Tea Set - from $30</p>
+</div>
+<div className='one'>
+<img src={pho} alt='prop' id='pic7'></img>
+<p id="era7">Tea Set - from $30</p>
+</div>
+</div>
+<div id='erra'>
+<HiArrowNarrowLeft id='ar' href='era7'  /> 
+<HiArrowNarrowRight id='ar' href='era7' />
+</div>
+</div>
+<div>
+
+
+</div>
+</div>
+  );
+}
+
+export default App;
